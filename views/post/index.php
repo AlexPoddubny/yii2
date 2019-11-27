2fd363b87502267f190376ab18b45d087071555b
AlexPoddubny <usatij.alex@gmail.com>
2019-11-28 00:28:52 +0200

    украшательства в post/index

--- views/post/index.php
+++ views/post/index.php
@@ -2,38 +2,39 @@
 	use yii\widgets\ActiveForm;
 	use yii\helpers\Html;
 ?>
-<h1>Action Test</h1>
-<?php if (Yii::$app->session->hasFlash('success')) : ?>
-	<div class="alert alert-success alert-dismissible" role="alert">
-		<?= Yii::$app->session->getFlash('success'); ?>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-<?php endif; ?>
-<?php if (Yii::$app->session->hasFlash('error')) : ?>
-	<div class="alert alert-danger alert-dismissible" role="alert">
-		<?= Yii::$app->session->getFlash('error'); ?>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-<?php endif; ?>
-
-<?php
-	$form = ActiveForm::begin([
-		'options' => [
-				'class' => 'form-horizontal',
-				'id' => 'myForm',
-			]
-		]);
-	echo $form->field($model, 'name');
-	echo $form->field($model, 'email')->input('email');
-	echo $form->field($model, 'text')->textarea([
-			'rows' => 10
-		]);
-	echo Html::submitButton('Отправить', [
-			'class' => 'btn btn-success'
-		]);
-	ActiveForm::end();
-?>
+<div class="container">
+	<h1>Action Test</h1>
+	<?php if (Yii::$app->session->hasFlash('success')) : ?>
+		<div class="alert alert-success alert-dismissible" role="alert">
+			<?= Yii::$app->session->getFlash('success'); ?>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+	<?php endif; ?>
+	<?php if (Yii::$app->session->hasFlash('error')) : ?>
+		<div class="alert alert-danger alert-dismissible" role="alert">
+			<?= Yii::$app->session->getFlash('error'); ?>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+	<?php endif; ?>
+	<?php
+		$form = ActiveForm::begin([
+			'options' => [
+					'class' => 'form-horizontal',
+					'id' => 'myForm',
+				]
+			]);
+		echo $form->field($model, 'name');
+		echo $form->field($model, 'email')
+			->input('email');
+		echo $form->field($model, 'text')
+			->textarea(['rows' => 10]);
+		echo Html::submitButton('Отправить', [
+				'class' => 'btn btn-success'
+			]);
+		ActiveForm::end();
+	?>
+</div>
